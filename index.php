b2a5c11bdea5dd7f9ce5d332aa25f24e0728e40a
hanifferdiansyah <haniffferdiansyah@gmail.com>
2024-11-23 12:13:54 +0700

    membuat halaman utama dan navbar

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project IS62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+<?php 
+include_once('navbar.php');
+?>
+
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header text-center">
+                <h3>ORDER TRACKER</h3>
+            </div>
+            <div class="card-body text-center" >
+                <h4>Selamat Datang, Pelanggan Lapar</h4>
+                <p>Di CHICKEN STRORE</p>
+
+                <p class=""><a class="btn btn-primary" href="/UTS_HANIFFERDIANSYAH/Data_pesanan/pesan.php"><i class="fa-solid fa-square-plus"></i> Pesan Sekarang</a></p>
+                
+
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="js/bootstrap.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
